fc0105a4cbd07f9503372fe9e704acdec997a071
sodalite1204 <your.jiny.9@gmail.com>
2023-03-05 00:34:19 +0900

    feat: Add theme color

---
+++ styles/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from 'styled-components/native';
+
+export const theme: DefaultTheme = {
+  white: '#FAFAFA',
+  olive20: '#EEF6D2',
+  olive40: '#CDDBC2',
+  olive60: '#A5B79D',
+  olive100: '#535939',
+  grey20: '#DBDBDB',
+  grey40: '#D1D5DB',
+  grey60: '#9CA3AF',
+  grey100: '#828282',
+  black: '#323232',
+};
